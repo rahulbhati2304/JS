26a18de2b3a64a803f4de4db4d54e49521c1ad01
rahulbhati2304 <66083004+rahulbhati2304@users.noreply.github.com>
2020-07-08 10:03:07 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,6 @@
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', () => {
+	myHeading.style.color = 'red';
+});
